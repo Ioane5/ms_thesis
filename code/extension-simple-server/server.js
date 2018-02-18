284f65badbc1d2ccaff0492b16ef09ad23aa3e39
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-02-18 21:57:34 +0200

    Implement Simple Extension For demonstratin Live Mode'

---
+++ code/extension-simple-server/server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+  	console.log('message: ' + msg)
+  	io.emit('message', msg);
+  });
+
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
