a32d4f35dc0ec1e7fce3b3f693df7bbe933d70bd
Ioane Sharvadze <ioane.sharvadze@gmail.com>
2018-03-13 22:09:19 +0200

    Design Update

--- code/extension/background.js
+++ code/extension/background.js
@@ -4,4 +4,4 @@ function randomToken() {
   return Math.floor((1 + Math.random()) * 1e16).toString(16).substring(1);
 }
 
-let dataController = new P2PController('key1', 'prvtk', 'http://localhost:8080');
+let dataController;
--- code/extension/popup.js
+++ code/extension/popup.js
@@ -1,26 +1,88 @@
-function getDataController() {
-    if (chrome.extension) { // If in chrome extension mode
-        return chrome.extension.getBackgroundPage().dataController;
-    } else { // else Just browser page mode
-        return new P2PController('key1', 'prvtk', 'http://localhost:8080');
-    }
-}
-
 $(function () {
-    var dataController = undefined;
-    $('#config_form').submit(() => {
-        dataController = new P2PController($('#my_public_key').val(), $('#my_private_key').val(), 'http://localhost:8080');
-        dataController.listenDataChanges((message) => {
+    let dataController = undefined;
+
+    function initDataController(publicKey, privateKey) {
+        let newDataController = new P2PController(publicKey, privateKey, 'http://localhost:8080');
+        if (chrome.extension) { // If in chrome extension mode
+            chrome.extension.getBackgroundPage().dataController = newDataController;
+        } else { // else Just browser page mode
+            dataController = newDataController;
+        }
+
+        newDataController.listenDataChanges((message) => {
             $('#messages').append($('<li>').text(message));
         });
-        console.log('here we are');
+    }
+
+    function getDataController() {
+        if (chrome.extension) { // If in chrome extension mode
+            return chrome.extension.getBackgroundPage().dataController;
+        } else { // else Just browser page mode
+            return dataController;
+        }
+    }
+
+    function initialize() {
+        if (getDataController()) {
+            fillSettings();
+        } else {
+            openSettingsModal();
+        }
+    }
+
+    /**
+     * Fills settings with Public and private keys
+     */
+    function fillSettings() {
+        let controller = getDataController();
+        $('#public_key').val(controller.publicKey);
+        $('#private_key').val(controller.privateKey);
+    }
+
+    $('#send_input').keypress(function (event) {
+        var keycode = event.keyCode || event.which;
+        if (keycode == '13') {
+            sendMessage();
+        }
+    });
+
+    $('#send_button').click(() => {
+        sendMessage();
         return false;
     });
-    $('#send_form').submit(() => {
-        let message = $('#send_input').val();
-        dataController.saveData(message, $('#peer_public_key').val());
+
+    function sendMessage() {
+        let sendInput = $('#send_input');
+        let message = sendInput.val();
+        getDataController().saveData(message, $('#peer_public_key').val());
         // Reset Value in input
-        $('#send_input').val('');
-        return false;
+        sendInput.val('');
+    }
+
+    $('#settings_button').click(() => {
+        openSettingsModal()
     });
+
+    function openSettingsModal() {
+        let settingsModalDialog = $('#settings_modal');
+        // settingsModalDialog.modal();
+        settingsModalDialog.modal('open');
+    }
+
+    $('#settings_modal').modal({
+            dismissible: false, // Modal can be dismissed by clicking outside of the modal
+            complete: function () {
+                let publicKey = $('#public_key').val();
+                let privateKey = $('#private_key').val();
+
+                if (publicKey && privateKey) {
+                    initDataController(publicKey, privateKey);
+                } else {
+                    openSettingsModal();
+                }
+            }
+        }
+    );
+
+    initialize();
 });
